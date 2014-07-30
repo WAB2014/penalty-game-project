9a4c2d513d6e8e703ee0116fbb77a67db77bb99f
WAB2014 <William03@Williams-MacBook-Air.local>
2014-07-30 12:02:00 +0100

    adding football reading file

---
+++ FOOTBALLreader.py
@@ -0,0 +1,8 @@
+import open('EO.csv', 'r') as file:
+    reader = csv.reader(file)
+    i = 0
+    for row in reader:
+        print(', '.join(row[1:7]))
+        if i == 5:
+            break
+        i += 1
